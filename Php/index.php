1d549f8fa41f83de6fd5a789c8cc71116de1eeef
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-09-29 18:17:50 +0700

    fix some style in html

--- Php/index.php
+++ Php/index.php
@@ -8,35 +8,55 @@
     <title>Document</title>
 </head>
 
-<body>
-    <form action="server/server.php" method="post">
-        <div class="container text-light">
-            <div>
-                <label class="mt-2 mb-1 text" for="">Name</label><br>
-                <input name="nama" class="border-0 w-100 rounded" type="text" autocomplete="off"
-                    placeholder="Your Name">
-            </div>
-            <div>
-                <label class="mb-1 text" for="">Email</label><br>
-                <input name="email" class="border-0 w-100 rounded" type="email" placeholder="youremail@email.com">
-            </div>
-            <div>
-                <label class="mb-1 text" for="">Message</label><br>
-                <textarea name="message" class="border-0 w-100 rounded" rows="3" type="text" autocomplete="off"
-                    placeholder="Text message here"></textarea>
-            </div>
-            <div class="container">
-                <div class="row">
-                    <div class="col">
-                        <img class="icon-img-about" src="assets/message-icon-flaticon.png" alt="">
+<style>
+    .ml-form{
+        margin-left:150px;
+    }
+    .tombol{
+        color: white;
+        font-size: 1.2rem;
+        margin-top: 1rem;
+        padding: .8rem 5rem;
+        border-radius: 3rem;
+    }
+    .pd-form{
+        padding: 10px;
+    }
+    .rounded-card{
+        border-radius: 2rem 0 2rem 2rem;
+    }
+</style>
+
+<body style="background-color: rgb(200, 200, 200);">
+    <section id="contact" class="vh-100 d-flex align-items-center">
+        <div class="container" style="width:600px">
+            <div class="card contact-card rounded-card py-5 rounded-top-right-0">
+                <h1 class="fw-bold justify-content-center d-flex mb-3">Contact</h1>
+                <form action="server.php" method="post">
+                    <div class="container">
+                        <div class="d-flex justify-content-center row">
+                            <label class="my-2 form-label ml-form" for="name">Name</label><br>
+                            <input id="name" name="nama" class="w-75 rounded pd-form" type="text" autocomplete="off"
+                                placeholder="Your Name">
+                        
+                            <label class="my-2 form-label ml-form" for="email">Email</label><br>
+                            <input id="email" name="email" class="w-75 rounded pd-form" type="email"
+                                placeholder="youremail@email.com">
+                        
+                            <label class="my-2 form-label ml-form" for="">Message</label><br>
+                            <textarea name="message" class="w-75 rounded pd-form" rows="3" type="text" autocomplete="off"
+                                placeholder="Text message here"></textarea>
+                        </div>
+
+                        <div class="col align-self-center d-flex justify-content-center">
+                            <input class="btn btn-primary tombol border-0" type="submit" value="Submit">
+                        </div>
                     </div>
-                    <div class="col align-self-center d-flex justify-content-end">
-                        <input class="custom-btn text-light border-0" type="submit" value="Submit">
-                    </div>
-                </div>
             </div>
+            </form>
         </div>
-    </form>
+        </div>
+    </section>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
--- Php/server.php
+++ Php/server.php
@@ -1,19 +1,20 @@
 <?php
-$nama = $_POST['nama'];
-$email = $_POST['email'];
-$message = $_POST['message'];
+$nama = htmlspecialchars($_POST['nama']);
+$email = htmlspecialchars($_POST['email']);
+$message = htmlspecialchars($_POST['message']);
+
 
 if (empty($nama) || empty($email) || empty($message)) {
     // Handle empty form fields, display an error message, or perform any other desired action.
     echo '<script>alert("Please fill in all the fields");</script>';
-    echo '<script>window.location.href = "../index.html#contact";</script>';
+    echo '<script>window.location.href = "index.php";</script>';
     exit();
 } else {
     // Construct the message to be sent to Discord
     $discordMessage = "PORTFOLIO CONTACT:\n\n";
-    $discordMessage .= "**Name:** $nama\n";
-    $discordMessage .= "**Email:** $email\n";
-    $discordMessage .= "**Message:**\n$message";
+    $discordMessage .= "Name: $nama\n";
+    $discordMessage .= "Email: $email\n";
+    $discordMessage .= "Message:\n$message";
 
     // Discord webhook URL
     $webhookUrl = 'https://discord.com/api/webhooks/1141671547107541073/d6eogkv7uATeQRBO5OgkA-N8sby0U54uNCBPANAfn0P_B7I1xOrdGZHtfLV2F7FRJySe';
@@ -43,12 +44,12 @@ if (empty($nama) || empty($email) || empty($message)) {
     if ($result === false) {
         // Handle the case where the request failed
         echo '<script>alert("Error sending the message");</script>';
-        echo '<script>window.location.href = "../index.html#contact";</script>';
+        echo '<script>window.location.href = "index.php";</script>';
         exit(); 
     } else {
         // Message sent successfully, you can display a success message or redirect the user
         echo '<script>alert("Message sent successfully");</script>';
-        echo '<script>window.location.href = "../index.html#contact";</script>';
+        echo '<script>window.location.href = "index.php";</script>';
         exit();
     }
 }
